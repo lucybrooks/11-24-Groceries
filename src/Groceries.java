12b8b475b1a4c7d9d7e4396041e3ea6c74b4eee4
lucybrooks <lucybrooks@users.noreply.github.com>
2014-11-25 08:15:58 -0700

    Update Groceries.java

--- src/Groceries.java
+++ src/Groceries.java
@@ -1,1 +1,22 @@
-// main program to run the GroceryList
+public class Groceries
+{
+  public static void main (String[]args)
+  {
+    GroceryList list=new GroceryList();
+    GroceryItem I1=new GroceryItem ("Oreos", 3, 2.5);
+    GroceryItem I2=new GroceryItem ("Goldfish Crackers", 2, 1.75);
+    GroceryItem I3=new GroceryItem ("Apple", 8, .23);
+    GroceryItem I4=new GroceryItem ("Fruit Loops", 2, 4.32);
+    GroceryItem I5=new GroceryItem ("Pop Tarts", 4, 3.67);
+    
+    list.add(I1);
+    list.add(I2);
+    list.add(I3);
+    list.add(I4);
+    list.add(I5);
+    
+    System.out.println("Grocery List");
+    System.out.println(list.toString());
+    System.out.println(list.getTotalCost());
+  }
+}
