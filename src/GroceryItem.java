a182e281b62563ef6d0a41eee0c5e277989df74a
lucybrooks <lucybrooks@users.noreply.github.com>
2014-11-25 08:16:42 -0700

    Update GroceryItem.java

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,1 +1,29 @@
-// class to represent a grocery item
+public class GroceryItem
+{
+  String name;
+  int quantity; 
+  double price;
+  
+  public GroceryItem (String name2, int quantity2, double price2)
+  {
+    name=name2;
+    quantity=quantity2;
+    price=price2;
+  }
+  
+  public double getCost()
+  {
+    double a=price*quantity;
+    return a;
+  }
+  
+  public void setQuantity(int newquantity)
+  {
+    quantity=newquantity;
+  }
+  
+  public String toString()
+  {
+    return quantity+" "+name.toString()+" @ "+price+" = "+getCost();
+  }
+}
